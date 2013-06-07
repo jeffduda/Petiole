c4669956f69a31308c5c3cbe57f7bbab580bec5c
Jeffrey Duda <jeff.duda@gmail.com>
2013-06-07 16:04:19 -0400

    ENH: renamed file

---
+++ PetioleToolkit.remote.cmake
@@ -0,0 +1,5 @@
+itk_fetch_module(NetworkToolkit
+      "Framework for network andalysis in medical images."
+        GIT_REPOSITORY https://bitbucket.org/jeffej/networktoolkit.git
+          GIT_TAG 9ea06c50b7c5
+          )
