1811759a94a716a811acdee688772dba59e4e039
Jeffrey Duda <jeff.duda@gmail.com>
2013-06-07 18:48:38 -0400

    ENH: update

--- PetioleToolkit.remote.cmake
+++ PetioleToolkit.remote.cmake
@@ -1,5 +1,5 @@
-itk_fetch_module(NetworkToolkit
-      "Framework for network andalysis in medical images."
-        GIT_REPOSITORY https://bitbucket.org/jeffej/networktoolkit.git
-          GIT_TAG 9ea06c50b7c5
+itk_fetch_module(PetioleToolkit
+      "Framework for network analysis in medical images."
+        GIT_REPOSITORY https://github.com/jeffduda/Petiole.git
+          GIT_TAG daa37ce27b
           )
