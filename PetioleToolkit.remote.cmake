afcd6e67111b175459d329cd43dc5c84f9b14243
Jeffrey Duda <jeff.duda@gmail.com>
2013-08-01 18:51:17 -0400

    ENH: library issues

--- PetioleToolkit.remote.cmake
+++ PetioleToolkit.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(PetioleToolkit
       "Framework for network analysis in medical images."
         GIT_REPOSITORY https://github.com/jeffduda/Petiole.git
-          GIT_TAG daa37ce27b
+          GIT_TAG 8d70801
           )
