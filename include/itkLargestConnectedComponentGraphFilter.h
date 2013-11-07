b0e346ebfa08d843d126654dbb68a02788d4bfdb
Jeffrey Duda <jeff.duda@gmail.com>
2013-11-07 18:00:05 -0500

    ADD: connected components

---
+++ include/itkLargestConnectedComponentGraphFilter.h
@@ -0,0 +1,123 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkLargestConnectedComponentGraphFilter_h
+#define __itkLargestConnectedComponentGraphFilter_h
+
+#include "itkObject.h"
+#include "itkObjectFactory.h"
+#include "itkVariableSizeMatrix.h"
+#include "itkDijkstrasDistanceMatrixGraphFilter.h"
+#include "itkVectorContainer.h"
+#include "itkGraphToGraphFilter.h"
+#include "itkExtractSubgraphFilter.h"
+#include "itkDijkstrasGraphTraits.h"
+
+namespace itk
+{
+/** \class LargestConnectedComponentGraphFilter
+ *  \brief Computes the minimum and the maximum intensity values of
+ *         an image.
+ *
+ * This calculator computes the minimum and the maximum intensity values of
+ * an image.  It is templated over input image type.  If only Maximum or
+ * Minimum value is needed, just call ComputeMaximum() (ComputeMinimum())
+ * otherwise Compute() will compute both.
+ *
+ * \ingroup Operators
+ * \ingroup ITKCommon
+ *
+ * \wiki
+ * \wikiexample{ImageProcessing/GraphNodeStrengthCalculator,Find the minimum and maximum value (and the position of the value) in an image}
+ * \wikiexample{Developer/OilPaintingImageFilter,Multi-threaded oil painting image filter}
+ * \endwiki
+ */
+template< class TInputGraph, typename TOutputGraph >
+class ITK_EXPORT LargestConnectedComponentGraphFilter:public GraphToGraphFilter<TInputGraph,TOutputGraph>
+{
+public:
+  /** Standard class typedefs. */
+  typedef LargestConnectedComponentGraphFilter                             Self;
+  typedef GraphToGraphFilter<TInputGraph,TOutputGraph>    Superclass;
+  typedef SmartPointer< Self >                            Pointer;
+  typedef SmartPointer< const Self >                      ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(LargestConnectedComponentGraphFilter, Object);
+
+  typedef float                                     ValueType;
+  typedef VectorContainer<unsigned long, ValueType> ValueVectorType;
+
+  typedef TInputGraph InputGraphType;
+
+  /** Pointer type for the image. */
+  typedef typename TInputGraph::Pointer InputGraphPointer;
+
+  typedef typename InputGraphType::NodeType InputNodeType;
+
+  typedef typename InputGraphType::RegionType::Pointer RegionPointer;
+
+  /** Const Pointer type for the image. */
+  typedef typename TInputGraph::ConstPointer InputGraphConstPointer;
+
+  /** Type definition for the input image. */
+  typedef TOutputGraph OutputGraphType;
+
+  /** Pointer type for the image. */
+  typedef typename TOutputGraph::Pointer OutputGraphPointer;
+
+  typedef typename OutputGraphType::NodeType NodeType;
+
+  typedef typename OutputGraphType::EdgeType EdgeType;
+
+  typedef typename OutputGraphType::NodePointerType NodePointerType;
+
+  typedef typename OutputGraphType::EdgePointerType EdgePointerType;
+
+  typedef itk::DijkstrasGraphTraits<typename InputGraphType::TraitsType::NodeWeightType,3> DijkstrasTraitsType;
+
+  typedef DijkstrasDistanceMatrixGraphFilter<InputGraphType> DistanceMatrixFilter;
+  
+  typedef ExtractSubgraphFilter<InputGraphType, OutputGraphType> ExtractFilterType;
+  
+  typedef typename ExtractFilterType::Pointer                    ExtractFilterPointer;
+
+  /** Prepare the output */
+  void GenerateOutputInformation( void );
+
+protected:
+  LargestConnectedComponentGraphFilter();
+  virtual ~LargestConnectedComponentGraphFilter() {}
+  void PrintSelf(std::ostream & os, Indent indent) const;
+
+private:
+  LargestConnectedComponentGraphFilter(const Self &);           //purposely not implemented
+  void operator=(const Self &);                //purposely not implemented
+
+  void GenerateData();
+
+};
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkLargestConnectedComponentGraphFilter.hxx"
+#endif
+
+#endif /* __itkLargestConnectedComponentGraphFilter_h */
---
+++ include/itkLargestConnectedComponentGraphFilter.hxx
@@ -0,0 +1,184 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkLargestConnectedComponentGraphFilter_hxx
+#define __itkLargestConnectedComponentGraphFilter_hxx
+
+#include "itkLargestConnectedComponentGraphFilter.h"
+#include "itkImageRegionConstIteratorWithIndex.h"
+#include "itkNumericTraits.h"
+#include "itkArray.h"
+
+namespace itk
+{
+/**
+ * Constructor
+ */
+template< class TInputImage, typename TValueType >
+LargestConnectedComponentGraphFilter< TInputImage, TValueType >
+::LargestConnectedComponentGraphFilter()
+{
+  this->ProcessObject::SetNumberOfRequiredInputs( 1 );
+
+  OutputGraphPointer output
+    = dynamic_cast<OutputGraphType*>( this->MakeOutput( 0 ).GetPointer() );
+
+  this->ProcessObject::SetNumberOfRequiredOutputs( 1 );
+  this->ProcessObject::SetNthOutput( 0, output.GetPointer() );
+
+}
+
+
+template <class TInputGraph, class TOutputGraph>
+void
+LargestConnectedComponentGraphFilter<TInputGraph,TOutputGraph>
+::GenerateData( void )
+{
+  OutputGraphPointer output = this->GetOutput();
+  typename InputGraphType::GraphTraitsType traits;  
+
+  typename DistanceMatrixFilter::Pointer distanceFilter = DistanceMatrixFilter::New();
+  distanceFilter->SetInput( this->GetInput() );
+  distanceFilter->Update();
+  typename DistanceMatrixFilter::MatrixType mat = distanceFilter->GetDistanceMatrix();
+  
+  std::cout << mat << std::endl;
+
+/*
+
+  
+  unsigned long nNodes = this->GetInput()->GetTotalNumberOfNodes();
+  unsigned long nEdges = this->GetInput()->GetTotalNumberOfEdges();
+  typename OutputGraphType::RegionType::Pointer subgraph = OutputGraphType::RegionType::New();
+  unsigned long idx = 0;
+  for ( unsigned int i=0; i<nNodes; i++)
+    {
+    if ( degrees->GetDegreeCentrality()->GetElement(i) > this->m_Level )
+      {
+      subgraph->InsertElement( idx, i );
+      ++idx;
+      } 
+    }
+
+  if ( subgraph->Size() < 2 )
+    {
+    this->m_RichClubCoefficient = 0;
+    }
+  else
+    {
+    std::vector<float> weights;
+    typename OutputGraphType::RegionType::Pointer subedges = OutputGraphType::RegionType::New();
+    idx = 0;
+    ValueType weightsum = 0.0;
+    for ( unsigned int i=0; i<nEdges; i++)
+      {
+      weights.push_back( this->GetInput()->GetEdge(i).Weight );
+      if ( ( degrees->GetDegreeCentrality()->GetElement( this->GetInput()->GetEdge( i ).SourceIdentifier ) > this->m_Level ) &&
+           ( degrees->GetDegreeCentrality()->GetElement( this->GetInput()->GetEdge( i ).TargetIdentifier ) > this->m_Level ) )
+        {
+        subedges->InsertElement(idx,i);
+        weightsum += this->GetInput()->GetEdge( i ).Weight;
+        ++idx;
+        }
+      }
+    
+    ValueType rcNodes = subgraph->Size();
+    ValueType rcEdges = subedges->Size();
+
+    if ( this->m_IsWeighted )
+      {
+      std::sort( weights.begin(), weights.end() );
+      ValueType normweighted = 0.0;
+      for (unsigned int i=(nEdges-subedges->Size()); i<nEdges; i++)
+        {
+        normweighted += weights[i];
+        }
+      this->m_RichClubCoefficient = weightsum / normweighted;
+      }
+    else
+      {
+      this->m_RichClubCoefficient = rcEdges / ( rcNodes * (rcNodes-1) ); 
+      }
+
+    }
+
+  if ( subgraph->Size() > 0 ) 
+    {
+
+
+    itk::Array<unsigned long> newNodeId;
+    newNodeId.SetSize( this->GetInput()->GetTotalNumberOfNodes() );
+
+    for ( unsigned int i=0; i<subgraph->Size(); i++ )
+      {
+      NodePointerType node = output->CreateNewNode();
+      newNodeId[ subgraph->GetElement(i) ] = node->Identifier;
+      traits.CopyNodeStaticData( this->GetInput()->GetNode( subgraph->GetElement( i ) ), node );
+      }
+    
+    for ( unsigned int i=0; i<this->GetInput()->GetTotalNumberOfEdges(); i++)
+      {
+      unsigned long source = this->GetInput()->GetEdge( i ).SourceIdentifier;
+      unsigned long target = this->GetInput()->GetEdge( i ).TargetIdentifier;
+      bool hasSource = false;
+      bool hasTarget = false;
+      for (unsigned int j=0; j<subgraph->Size(); j++)
+        {
+        if ( source == subgraph->GetElement( j ) )
+          {
+          hasSource = true;
+          }
+        if ( target == subgraph->GetElement( j ) )
+          {
+          hasTarget = true;
+          }
+        }
+      if ( hasSource && hasTarget )
+        {
+        EdgePointerType edge = output->CreateNewEdge( newNodeId[source], newNodeId[target] );
+        traits.CopyEdgeStaticData( this->GetInput()->GetEdge( i ), edge );
+        }
+      }
+    }
+  else
+    {
+    itkExceptionMacro( "itk::LargestConnectedComponentGraphFilter::GenerateData() needs an Region to extract " );
+    }
+*/
+}
+
+template <class TInputGraph, class TOutputGraph>
+void
+LargestConnectedComponentGraphFilter<TInputGraph,TOutputGraph>
+::GenerateOutputInformation( void )
+{
+}
+
+template< class TInputImage, typename TValueType >
+void
+LargestConnectedComponentGraphFilter< TInputImage, TValueType >
+::PrintSelf(std::ostream & os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+
+  os << indent << "LargestConnectedComponentGraphFilter: "
+     << std::endl;
+  //os << indent << "Region set by User: " << m_RegionSetByUser << std::endl;
+}
+} // end namespace itk
+
+#endif
